c22d55f646d32c6a0c89152716b376251ec86eea
Yirga Mulaw <infoyirga12@gmail.com>
2023-04-30 20:41:26 +0300

    Test Cases update

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Test BaseModel for expected behavior and documentation"""
+
 from datetime import datetime
 import inspect
 import models
@@ -7,6 +8,7 @@ import pep8 as pycodestyle
 import time
 import unittest
 from unittest import mock
+
 BaseModel = models.base_model.BaseModel
 module_doc = models.base_model.__doc__
 
